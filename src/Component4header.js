0da4772814c8d7b2a50a569034dafd84f3ad16b6
unknown <you@example.com>
2023-07-10 12:27:13 +0530

    edit

--- src/Component4header.js
+++ src/Component4header.js
@@ -4,11 +4,15 @@ import "./Component4header.css"
 function Component4header(props){
     return(
         <>
+        <div className="first-parent">
+            <div className="border">
         <img src={props.image} className="image"></img>
+        </div>
         <div className="content">
         <h1 className="heading">{props.name}</h1>
         <p className="para">{props.description}</p>
         <button className="btn2">{props.text}</button>
+          </div>
           </div>
 
          
--- src/Component4header1.js
+++ src/Component4header1.js
@@ -3,11 +3,15 @@ import "./Component4header1.css"
 function Component4header1(props){
     return(
         <>
+        <div className="second-parent">
+            <div className="border1">
         <img src={props.image1} className="image1"></img>
+        </div>
         <div className="second">
         <h1 className="heading1">{props.name1}</h1>
         <p className="para1">{props.description1}</p>
         <button className="btn1">{props.text1}</button>
+        </div>
         </div>
 
         </>
