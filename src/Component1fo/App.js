4af6e32a5cb1032f760df8b6dc1b4238f64c8b5b
unknown <you@example.com>
2023-07-24 20:33:32 +0530

    component1

---
+++ src/Component1fo/App.js
@@ -0,0 +1,38 @@
+import React from "react"
+
+import Componen1 from "./Component1"
+
+
+
+function App(){
+  return(
+    <div className="appdiv">
+
+  
+  
+    
+    
+   
+    <Componen1 title="1" name="dilsha" name1="rinsha" />
+
+    
+
+
+
+
+      
+     
+      
+
+
+
+
+   
+
+
+    </div>
+  )
+}
+
+
+export default App
