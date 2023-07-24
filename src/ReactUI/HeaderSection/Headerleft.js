dad476c21662e37a0e9fb680ff2c01e3fae83c08
unknown <you@example.com>
2023-07-24 20:13:48 +0530

    ReactUI

---
+++ src/ReactUI/HeaderSection/Headerleft.js
@@ -0,0 +1,9 @@
+import React from "react";
+function Headerleft(){
+    return(
+        <div className="logo">
+        <h1><img src="https://img.freepik.com/free-icon/mac-os_318-10374.jpg" className="apple" alt=""></img>APPLE</h1>
+        </div>
+    )
+}
+export default Headerleft;
