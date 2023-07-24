80cbd7dbeae248af9fd64fc037aaba0bc7ac4569
unknown <you@example.com>
2023-07-25 00:54:57 +0530

    state

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import ReactDOM from 'react-dom/client';
 
 import App from './Component5/Component5/src/Component5/App';
 import Counter from './Counter';
+import State from './State';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  
-    <Counter/>
+    <State/>
     
     
   
