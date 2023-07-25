bfdf71cfc6dec30ffe05ce3975bab4b54250d56f
dilshaf <136444564+dilshaf@users.noreply.github.com>
2023-07-25 16:00:18 +0530

    Delete src/ReactUI directory

--- src/ReactUI/HeaderSection/Headerleft.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-function Headerleft(){
-    return(
-        <div className="logo">
-        <h1><img src="https://img.freepik.com/free-icon/mac-os_318-10374.jpg" className="apple" alt=""></img>APPLE</h1>
-        </div>
-    )
-}
-export default Headerleft;
